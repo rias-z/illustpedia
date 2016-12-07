9387db2bccac79a4a42eca94c704070eda1b9afa
chestermx <chestermx88@gmail.com>
2016-12-08 00:08:43 +0900

    アカウント作成機能追加

--- illustpedia/general/views.py
+++ illustpedia/general/views.py
@@ -1,8 +1,18 @@
 from django.views import generic
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import IPUser
+
+
+# form
+class AccountCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = IPUser
+        fields = ("username", "nickname", "email")
 
 
 class IndexView(generic.TemplateView):
@@ -26,8 +36,23 @@ class LogoutView(generic.View):
         return redirect('general:login')
 
 
-class AccountCreateView(generic.TemplateView):
+class AccountCreateView(generic.CreateView):
     template_name = 'I002_account_create.html'
+    form_class = AccountCreateForm
+    success_url = reverse_lazy('general:top')
+
+    def done(self, request, cleaned_data):
+        obj = AccountCreateForm(request.POST).save(commit=False)
+        obj.created_by = self.request.user
+        obj.save()
+        # user = authenticate(username=cleaned_data['username'],
+        #                     password=cleaned_data['password1'])
+        # login(self.request, user)
+        # return redirect(reverse('general:top', args=(), kwargs={}))
+
+    def get_success_url(self):
+            return reverse_lazy('general:login')
+
 
 class TopView(generic.TemplateView):
     template_name = 'I003_top.html'
